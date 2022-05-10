f4dde87bbb584ebf78a7c0241bdd3b6b86290853
Null-Root <triascend94@gmail.com>
2022-05-10 14:04:18 +0800

    Add: Fragment Host and Main Menu

--- AndroidApp/app/src/main/java/com/tuna_salmon/androidapp/MainFragment.java
+++ AndroidApp/app/src/main/java/com/tuna_salmon/androidapp/MainFragment.java
@@ -1,64 +1,155 @@
 package com.tuna_salmon.androidapp;
 
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MainFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class MainFragment extends Fragment {
+import com.tuna_salmon.androidapp.services.AppService;
+import com.tuna_salmon.androidapp.services.ServiceCollection;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import java.util.Objects;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class MainFragment extends Fragment implements View.OnClickListener {
+    private AppService.Data appDataService;
+    private AppService.UI appUIService;
 
-    public MainFragment() {
-        // Required empty public constructor
-    }
+    private NavController navController;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MainFragment newInstance(String param1, String param2) {
-        MainFragment fragment = new MainFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    private TextView ID_Type_Holder;
+    private TextView ID_Holder;
+
+    private TextView DataSourceDisplay;
+
+    private int isExitApp;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+
+                isExitApp++;
+
+                if(isExitApp == 1) {
+                    // Prompt to Exit
+                }
+
+                new Handler().postDelayed(() -> {
+                    if(isExitApp >= 2) {
+                        // EXIT APP
+                        requireActivity().finishAffinity();
+                    }
+                    else {
+                        isExitApp = 0;
+                    }
+                }, 2500);
+            }
+        };
+        requireActivity().getOnBackPressedDispatcher().addCallback(
+                this, // LifecycleOwner
+                callback);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        appDataService = ServiceCollection.getServiceCollection().getAppService().getAppDataService();
+        appUIService = ServiceCollection.getServiceCollection().getAppService().getAppUIService();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+
+        ID_Type_Holder = view.findViewById(R.id.mainMenuShowIDType);
+        ID_Holder = view.findViewById(R.id.mainMenuShowID);
+        DataSourceDisplay = view.findViewById(R.id.mainFragmentDataSourceDisplay);
+
+        switch (Objects.requireNonNull(appDataService.getAppData().get(Const.Device.DATA_SOURCE))) {
+            case Const.DataSource.OFFLINE:
+                DataSourceDisplay.setTextColor(ColorStateList.valueOf(Color.parseColor("#f23524")));
+                DataSourceDisplay.setText(Const.DataSource.OFFLINE);
+                ID_Type_Holder.setText("");
+                ID_Holder.setText("");
+                break;
+            case Const.DataSource.ONLINE:
+                DataSourceDisplay.setTextColor(ColorStateList.valueOf(Color.parseColor("#4dffa2")));
+                DataSourceDisplay.setText(Const.DataSource.ONLINE);
+                ID_Type_Holder.setText(appDataService.getAppData().get(Const.Device.ID_TYPE));
+                ID_Holder.setText(appDataService.getAppData().get(Const.Device.ID));
+                break;
+        }
+
+        view.findViewById(R.id.mainInventoryView).setOnClickListener(this);
+        view.findViewById(R.id.mainMultiEditView).setOnClickListener(this);
+        view.findViewById(R.id.mainDashboardView).setOnClickListener(this);
+        view.findViewById(R.id.mainSettingsView).setOnClickListener(this);
+        view.findViewById(R.id.mainCreditsView).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.mainInventoryView:
+                // Check For ID Before Entering
+                if(allowNavAction()) {
+                    navController.navigate(R.id.action_mainFragment_to_categoryFragment);
+                }
+                break;
+            case R.id.mainMultiEditView:
+                // Check For ID Before Entering
+                if(allowNavAction()) {
+                    navController.navigate(R.id.action_mainFragment_to_bulkEditFragment);
+                }
+                break;
+            case R.id.mainDashboardView:
+                navController.navigate(R.id.action_mainFragment_to_dashboardFragment);
+                break;
+            case R.id.mainSettingsView:
+                navController.navigate(R.id.action_mainFragment_to_settingsFragment);
+                break;
+            case R.id.mainCreditsView:
+                navController.navigate(R.id.action_mainFragment_to_creditsFragment);
+        }
+    }
+
+    private boolean allowNavAction() {
+        switch (Objects.requireNonNull(appDataService.getAppData().get(Const.Device.DATA_SOURCE))) {
+            case Const.DataSource.ONLINE:
+            {
+                if(appDataService.isAnyIDUsed())
+                    return true;
+                else {
+                    appUIService.simpleDialog("Nav Error", "No ID Used!");
+                    return false;
+                }
+            }
+            case Const.DataSource.OFFLINE:
+                return true;
+        }
+        return false;
     }
 }
