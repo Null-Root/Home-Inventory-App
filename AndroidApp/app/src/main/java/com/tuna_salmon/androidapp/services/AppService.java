3ca8dcb88a4979b3ccd4ed1b2073dfae241c2bd5
Null-Root <triascend94@gmail.com>
2022-05-11 01:39:15 +0800

    Remove: sync keyword in AppService

--- AndroidApp/app/src/main/java/com/tuna_salmon/androidapp/services/AppService.java
+++ AndroidApp/app/src/main/java/com/tuna_salmon/androidapp/services/AppService.java
@@ -88,7 +88,39 @@ public class AppService {
 
         private SharedPreferences LocalData = null;
 
-        public synchronized Map<String, String> getAppData() {
+        public void Initialize() {
+            LocalData = ctx.getSharedPreferences(Const.SharedPrefs.USER_DATA, Context.MODE_PRIVATE);
+
+            // Check For Name
+            if (!LocalData.contains(Const.Device.NAME))
+                LocalData.edit().putString(Const.Device.NAME, "<DEF_USER>").apply();
+            // Check For Email
+            if (!LocalData.contains(Const.Device.EMAIL))
+                LocalData.edit().putString(Const.Device.EMAIL, null).apply();
+            // Check For Password
+            if(!LocalData.contains(Const.Device.PASSWORD))
+                LocalData.edit().putString(Const.Device.PASSWORD, null).apply();
+            // Check For UserID
+            if (!LocalData.contains(Const.ID_Type.USER_ID))
+                LocalData.edit().putString(Const.ID_Type.USER_ID, null).apply();
+            // Check For Custom ID
+            if (!LocalData.contains(Const.ID_Type.CUSTOM_ID))
+                LocalData.edit().putString(Const.ID_Type.CUSTOM_ID, null).apply();
+            // Check For ID Last Chosen
+            if (!LocalData.contains(Const.Device.ID_TYPE))
+                LocalData.edit().putString(Const.Device.ID_TYPE, null).apply();
+            // Check For Data Source
+            if (!LocalData.contains(Const.Device.DATA_SOURCE))
+                LocalData.edit().putString(Const.Device.DATA_SOURCE, Const.DataSource.ONLINE).apply();
+
+
+            // Set Singleton Variables
+            this.CurrentName = LocalData.getString(Const.Device.NAME, null);
+            this.CurrentIDType = LocalData.getString(Const.Device.ID_TYPE, null);
+            this.CurrentID = LocalData.getString(this.CurrentIDType, null);
+        }
+
+        public Map<String, String> getAppData() {
             Map<String, String> ret_val = new HashMap<>();
             ret_val.put(Const.Device.ID_TYPE, LocalData.getString(Const.Device.ID_TYPE, null));
             ret_val.put(Const.Device.ID, CurrentID);
@@ -98,7 +130,27 @@ public class AppService {
             return ret_val;
         }
 
-        public synchronized boolean isAnyIDUsed() {
+        public <T> void setAppData(String Ref_Name, T Data) {
+            // Get SharedPreferences Editor
+            SharedPreferences.Editor editor = LocalData.edit();
+
+            if (Data instanceof String)
+                editor.putString(Ref_Name, (String) Data);
+            else if (Data instanceof Integer)
+                editor.putInt(Ref_Name, (Integer) Data);
+            else if (Data instanceof Boolean)
+                editor.putBoolean(Ref_Name, (Boolean) Data);
+            else if (Data instanceof Float)
+                editor.putFloat(Ref_Name, (Float) Data);
+            else if (Data instanceof Long)
+                editor.putLong(Ref_Name, (Long) Data);
+
+            editor.apply();
+
+            onMainDataChanged();
+        }
+
+        public boolean isAnyIDUsed() {
             if(CurrentID != null) {
                 if(!CurrentID.equals("")) {
                     return true;
@@ -108,19 +160,27 @@ public class AppService {
         }
 
         public void onMainDataChanged() {
+            // Load to Storage
             LocalData.edit()
                     .putString(CurrentIDType, CurrentID)
                     .putString(Const.Device.ID_TYPE, CurrentIDType)
                     .putString(Const.Device.NAME, CurrentName)
                     .apply();
+
+            // Load to Variables
+
         }
 
-        public synchronized void setIDInstance(String ID, String IDType) {
+        public void setIDInstance(String ID, String IDType) {
             this.CurrentID = ID;
             this.CurrentIDType = IDType;
 
             // Call On Data Change
             onMainDataChanged();
+        }
+
+        public boolean isAccountRegistered() {
+            return false;
         }
     }
 
